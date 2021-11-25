36f0c1738f3eae2a1d8f2d52632699bf8d256560
Athorfeo <juanortizbayona@gmail.com>
2021-11-25 01:52:54 -0500

    Add logo

--- src/app/App.js
+++ src/app/App.js
@@ -2,10 +2,6 @@ import { Outlet, Link } from "react-router-dom";
 
 import './App.css';
 
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import Container from 'react-bootstrap/Container';
-
 import Navigation from '../component/Navigation';
 
 function App() {
--- src/component/Navigation.js
+++ src/component/Navigation.js
@@ -10,19 +10,18 @@ import logo from '../res/drawable/logo.svg';
 function Navigation() {
   return (
     <div>
-      <Navbar bg="light" expand="lg">
-        <Container className="bg-primary">
-          <Navbar.Brand as={Link} to="/"><img src={logo} className="App-logo" alt="logo" /> TUCASA | CONSTRUCCIONES</Navbar.Brand>
+      <Navbar expand="lg">
+        <Container>
+          <Navbar.Brand as={Link} to="/"><img src={logo} width="60"height="60" className="d-inline-block align-top" alt="logo" /></Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
+          <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+            <Nav>
               <Nav.Link as={Link} to="/">Home</Nav.Link>
-              <Nav.Link as={Link} to="portfolio">Portfolio</Nav.Link>
-              <Nav.Link as={Link} to="about">About</Nav.Link>
               <NavDropdown title="Portfolio" id="basic-nav-dropdown">
                 <NavDropdown.Item as={Link} to="portfolio">Gran Reserva</NavDropdown.Item>
                 <NavDropdown.Item as={Link} to="portfolio">Gran Reserva II</NavDropdown.Item>
               </NavDropdown>
+              <Nav.Link as={Link} to="about">About</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
